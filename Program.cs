92f3ce11a2b3ab0e2e05bdc2add82f85fbc56bdf
arman <armans8282@gmail.com>
2022-11-06 14:12:10 +0330

    Handled any possible exception

--- Program.cs
+++ Program.cs
@@ -8,48 +8,55 @@ int guesses;
 int guess;
 string response;
 
-do
+try
 {
-
-    guess = 0;
-    guesses = 0;
-    response = "";
-    var number = random.Next(1, 100);
-
     do
     {
-        Console.WriteLine("Guess a number between 1 to 100");
-        guess = Convert.ToInt32(Console.ReadLine());
 
-        if (number > guess)
+        guess = 0;
+        guesses = 0;
+        response = "";
+        var number = random.Next(1, 100);
+
+        do
         {
-            Console.WriteLine("You guessed too low, guess higher");
-        }
-        else if (number < guess)
-        {
-            Console.WriteLine("You guessed too high, guess lower");
-        }
-        guesses++;
-    } while (guess != number);
+            Console.WriteLine("Guess a number between 1 to 100");
+            guess = Convert.ToInt32(Console.ReadLine());
 
-    Console.WriteLine("You guessed the right number!");
-    Console.WriteLine("number: " + number);
-    Console.WriteLine("guesses: " + guesses);
-    playAgain = false;
+            if (number > guess)
+            {
+                Console.WriteLine("You guessed too low, guess higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("You guessed too high, guess lower");
+            }
+            guesses++;
+        } while (guess != number);
 
-    Console.WriteLine("would you like to play again? (Y/N)");
-    response = Console.ReadLine();
-    response = response.ToUpper();
-
-    if (response == "Y")
-    {
-        playAgain = true;
-    }
-    else if (response == "N")
-    {
+        Console.WriteLine("You guessed the right number!");
+        Console.WriteLine("number: " + number);
+        Console.WriteLine("guesses: " + guesses);
         playAgain = false;
-    }
 
-} while (playAgain);
+        Console.WriteLine("would you like to play again? (Y/N)");
+        response = Console.ReadLine();
+        response = response.ToUpper();
+
+        if (response == "Y")
+        {
+            playAgain = true;
+        }
+        else if (response == "N")
+        {
+            playAgain = false;
+        }
+
+    } while (playAgain);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 Console.WriteLine("Thank You For Playing");
 Console.ReadKey();
